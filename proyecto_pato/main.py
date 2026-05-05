03500e6610fec5d28e1c00e94c535da91bdf48e1
jeshu1234 <119417153+jeshux1234@users.noreply.github.com>
2026-05-04 21:35:36 -0400

    Add files via upload

---
+++ proyecto_pato/main.py
@@ -0,0 +1,4 @@
+from alumno import Alumno
+from encontrar_aluno import encontrar_alumno
+from registro_g import registro_g
+
---
+++ proyecto_pato/registro_g.py
@@ -0,0 +1,13 @@
+regitro_globlal={"1ro Basico": {},
+    "2do Basico": {},
+    "3ro Basico": {},
+    "4to Basico": {},
+    "5to Basico": {},
+    "6to Basico": {},
+    "7mo Basico": {},
+    "8vo Basico": {},
+    "1ro Medio": {},
+    "2do Medio": {},
+    "3ro Medio": {},
+    "4to Medio": {}
+}
