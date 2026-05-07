bcf44c49d143de7a170994db47bf7610f3fe066a
jeshu1234 <119417153+jeshux1234@users.noreply.github.com>
2026-05-07 19:45:40 -0400

    Delete proyecto_pato directory

--- proyecto_pato/alumno.py
+++
@@ -1,7 +0,0 @@
-class Alumno:
-    def __init__(self, rut, nombre, apellido, curso):
-        self.rut = rut
-        self.nombre = nombre
-        self.apellido = apellido
-        self.curso = curso
-        self.password = None
--- proyecto_pato/main.py
+++
@@ -1,4 +0,0 @@
-from alumno import Alumno
-from encontrar_aluno import encontrar_alumno
-from registro_g import registro_g
-
